99c7e42647a2f35de6ea5f90718c1e1bc429cbbb
Bruno Manika <112494341+BrunoIanoski@users.noreply.github.com>
2025-08-22 16:44:10 -0300

    Add files via upload

--- Exercícios_2/Ex_03.c
+++ Exercícios_2/Ex_03.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
 
-void procedimento()
+void procedimento(int vetor[], int n, int valor){
+    int i;
+    printf("Os elementos do vetor sao:\n");
+    for (i = 0; i < n; i++) {
+        vetor[i] = valor;
+        printf("[%d]", vetor[i]);
+    }
+    printf("\n");
 
+}
 
 
 int main(){
-    int i;
-    int n = 5;
+    int n = 5;  
     int vetor[5];
+    int valor;
 
-    
+    printf("Digite o numero que deseja preencher: ");
+    scanf("%d", &valor);
+
+    procedimento(vetor,n, valor);
 
     return 0;
 }
